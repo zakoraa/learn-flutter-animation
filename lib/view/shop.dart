9b3debd497719ed200d4086109343c7ab7643da2
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-11-15 20:49:48 +0700

    .

--- lib/view/shop.dart
+++ lib/view/shop.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animation/util/media_query.dart';
 
@@ -10,6 +11,13 @@ class ShopView extends StatefulWidget {
 
 class _ShopViewState extends State<ShopView> {
   late ScrollController _scrollController;
+  int _selectedIndex = 0;
+
+  final List<String> _carouselImages = [
+    "assets/ticket1.jpg",
+    "assets/ticket2.jpg",
+    "assets/ticket3.jpg",
+  ];
 
   @override
   void initState() {
@@ -27,14 +35,98 @@ class _ShopViewState extends State<ShopView> {
     super.dispose();
   }
 
+  void selectIndex(int index) {
+    _selectedIndex = index;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       height: getHeight(context),
       width: getWidth(context),
-      alignment: Alignment.center,
-      color: Colors.blue,
-      child: const Text("Halaman Shop"),
+      child: Stack(
+        children: [
+          SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 100),
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  width: getWidth(context),
+                  color: const Color.fromARGB(255, 226, 236, 255),
+                  child: Row(
+                    children: [
+                      Column(
+                        children: List.generate(
+                            _carouselImages.length,
+                            (index) => Container(
+                                  height: index == _selectedIndex ? 13 : 8,
+                                  width: index == _selectedIndex ? 13 : 8,
+                                  margin: const EdgeInsets.only(
+                                      top: 10, bottom: 10, right: 15),
+                                  decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: index == _selectedIndex
+                                          ? Colors.blueAccent
+                                          : const Color.fromARGB(
+                                              255, 183, 182, 182)),
+                                )),
+                      ),
+                      Expanded(
+                        child: CarouselSlider.builder(
+                          itemCount: _carouselImages.length,
+                          itemBuilder: (context, index, realIndex) => Container(
+                            margin: const EdgeInsets.symmetric(vertical: 20),
+                            clipBehavior: Clip.hardEdge,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Image.asset(
+                              _carouselImages[index],
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          options: CarouselOptions(
+                              onPageChanged: (index, reason) =>
+                                  selectIndex(index),
+                              autoPlay: true,
+                              scrollDirection: Axis.vertical,
+                              autoPlayAnimationDuration:
+                                  const Duration(milliseconds: 1000),
+                              enlargeCenterPage: true,
+                              scrollPhysics: const BouncingScrollPhysics(),
+                              viewportFraction: 1,
+                              aspectRatio: 4 / 3),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+              top: 0,
+              child: SafeArea(
+                child: Container(
+                  padding: const EdgeInsets.all(15),
+                  width: getWidth(context),
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      border: Border(
+                          bottom: BorderSide(
+                              width: 1,
+                              color: Color.fromARGB(255, 185, 185, 185)))),
+                  child: const Text(
+                    "Shop",
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 23),
+                  ),
+                ),
+              ))
+        ],
+      ),
     );
   }
 }
